05791d63c3092c6a9e5206fa36f05f335464ab1e
Alexandr Usenko <usenko.alexandr97@mail.ru>
2022-07-19 17:28:04 +0300

    my tgbot

--- tgbot_todo-list.py
+++ tgbot_todo-list.py
@@ -1,46 +1,45 @@
-import random
+import telebot
+
+bot = telebot.TeleBot('5426131943:AAFZUjAZOtkGnwgtZsavbK57OAi55Kfl6ts')
 
 HELP = """
 Список доступных команд:
-help - напечатать справку по программе
-add - добавить задачу в список(название задачи запрашивается у пользователя)
-show - показать добавленные задачи
-random - добавить случайную задачу дату Сегодня
+/help - вывести список доступных команд
+/todo - добавить задачу
+/show - показать все задачи на заданную дату
+/random - добавить случайную задачу дату
 """
-tasks = {
-}
-random_tasks = ['Записаться', 'Написать', 'Покормить', 'Помыть']
+
+todos = dict()
 
 def add_todo(date, task):
-    if date in tasks:
-        tasks[date].append(task)
+    date = date.lower()
+    if todos.get(date) is not None:
+        todos[date].append(task)
     else:
-        tasks[date] = []
-        tasks[date].append(task)
-    print(f'Задача {task} добавлена на дату {date}')
+        todos[date] = [task]
 
-while True:
-    command = input('Введите команду\n')
-    if command == 'help':
-        print(HELP)
-    elif command == 'add':
-        date = input('Введите дату для добавления задачи: ')
-        task = input('Введите название задачи: ')
-        add_todo(date, task)
-    elif command == 'show':
-        date = input('Введите дату для отображения списка задач: ')
-        if date in tasks:
-            for task in tasks[date]:
-                print('-', task)
-        else:
-            print('Такой даты нет')
-    elif command == 'random':
-        task = random.choice(random_tasks)
-        add_todo('Сегодня', task)
-    elif command == 'exit':
-        print('Спасибо за использования! До свидания!')
-        break
-    else:
-        print('Неизвестная команда')
-        break
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, HELP)
 
+@bot.message_handler(commands=['add'])
+def add(message):
+    _, date, tail = message.text.split(maxsplit=2)
+    task = ' '.join([tail])
+    add_todo(date, task)
+    bot.send_message(message.chat.id, f'Задача {task} добавлена на дату {date}')
+
+@bot.message_handler(commands=['show'])
+def show(message):
+    dates = message.text.split(maxsplit=1)[1].lower().split()
+    response = ''
+    for date in dates:
+        tasks = todos.get(date)
+        response += f'{date}: \n'
+        for task in tasks:
+            response += f'[] {task}\n'
+        response += '\n'
+    bot.send_message(message.chat.id, response)
+
+bot.polling(none_stop=True)
